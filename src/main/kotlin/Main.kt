1a7dc52c0d8066088fa395bb6ec0e9f4d400077d
Dmitry <boetz@yandex.ru>
2023-10-17 23:51:17 +0700

    HomeWork2_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,32 @@
-fun main () {
+fun main() {
 
-    var result = calcFee(10000, "Visa")
+    var result = calcFee(10_000, previousTransfersSumMonth = 35000)
     println(result)
 }
 
-fun calcFee (transferAmount:Int, cardType:String = "VK Pay", previousTransfersSum:Int = 0): Any {
+fun calcFee(transferAmount: Int, cardType: String = "VK Pay", previousTransfersSumMonth: Int = 0): Any {
 
     val feeRateMasterMaestro = 0.006
     val feeRateMinMasterMaestro = 20
-    val limit = 75_000
+    val limitForNoFeeMasterMaestro = 75_000
+    val monthlyLimitMasterMaestro = 600_000
+    val dailyLimitMasterMaestro = 150_000
     val feeRateVisaMir = 0.0075
     val feeRateMinVisaMir = 35
+    val dailyLimitVkpay = 15_000
+    val monthlyLimitVkpay = 40_000
 
     return when (cardType) {
-        "Visa" -> (if ((transferAmount * feeRateVisaMir) > feeRateMinVisaMir) transferAmount * feeRateVisaMir
+        "Visa", "Mir" -> (if ((transferAmount * feeRateVisaMir) > feeRateMinVisaMir) transferAmount * feeRateVisaMir
         else feeRateMinVisaMir)
-        "Mir" -> (if ((transferAmount * feeRateVisaMir) > feeRateMinVisaMir) transferAmount * feeRateVisaMir
-        else feeRateMinVisaMir)
-        "Maestro" -> (if (previousTransfersSum > limit) (transferAmount * feeRateMasterMaestro + feeRateMinMasterMaestro) else 0)
-
-        "Master" -> (if (previousTransfersSum > limit) (transferAmount * feeRateMasterMaestro + feeRateMinMasterMaestro) else 0)
-
-        else -> 0
+        "Maestro", "Master" -> if (transferAmount > dailyLimitMasterMaestro ||
+            (transferAmount + previousTransfersSumMonth) > monthlyLimitMasterMaestro
+        ) "Лимит переводов превышен" else
+            (if (previousTransfersSumMonth + transferAmount > limitForNoFeeMasterMaestro)
+                (transferAmount * feeRateMasterMaestro + feeRateMinMasterMaestro) else 0)
+        else -> if (transferAmount > dailyLimitVkpay ||
+            transferAmount + previousTransfersSumMonth > monthlyLimitVkpay
+        ) "Лимит переводов превышен" else 0
 
     }
 }
