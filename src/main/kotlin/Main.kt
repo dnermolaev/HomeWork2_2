7be187800733c528842cb718a5248768e91dc164
Dmitry <boetz@yandex.ru>
2023-10-18 23:50:37 +0700

    HomeWork2_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,29 +4,31 @@ fun main() {
     println(result)
 }
 
-fun calcFee(transferAmount: Int, cardType: String = "VK Pay", previousTransfersSumMonth: Int = 0): Any {
+fun calcFee(transferAmount: Int, cardType: String = "VK Pay", previousTransfersSumMonth: Int = 0): Number {
 
     val feeRateMasterMaestro = 0.006
     val feeRateMinMasterMaestro = 20
     val limitForNoFeeMasterMaestro = 75_000
-    val monthlyLimitMasterMaestro = 600_000
-    val dailyLimitMasterMaestro = 150_000
+    val monthlyLimit = 600_000
+    val dailyLimit = 150_000
     val feeRateVisaMir = 0.0075
     val feeRateMinVisaMir = 35
     val dailyLimitVkpay = 15_000
     val monthlyLimitVkpay = 40_000
 
     return when (cardType) {
-        "Visa", "Mir" -> (if ((transferAmount * feeRateVisaMir) > feeRateMinVisaMir) transferAmount * feeRateVisaMir
-        else feeRateMinVisaMir)
-        "Maestro", "Master" -> if (transferAmount > dailyLimitMasterMaestro ||
-            (transferAmount + previousTransfersSumMonth) > monthlyLimitMasterMaestro
-        ) "Лимит переводов превышен" else
+        "Visa", "Mir" -> if (transferAmount > dailyLimit ||
+            (transferAmount + previousTransfersSumMonth) > monthlyLimit
+        ) (-1) else
+            (if ((transferAmount * feeRateVisaMir) > feeRateMinVisaMir) transferAmount * feeRateVisaMir
+            else feeRateMinVisaMir)
+        "Maestro", "Master" -> if (transferAmount > dailyLimit ||
+            (transferAmount + previousTransfersSumMonth) > monthlyLimit
+        ) (-1) else
             (if (previousTransfersSumMonth + transferAmount > limitForNoFeeMasterMaestro)
                 (transferAmount * feeRateMasterMaestro + feeRateMinMasterMaestro) else 0)
         else -> if (transferAmount > dailyLimitVkpay ||
             transferAmount + previousTransfersSumMonth > monthlyLimitVkpay
-        ) "Лимит переводов превышен" else 0
-
+        ) (-1) else 0
     }
 }
